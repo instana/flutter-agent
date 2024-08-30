ed31bf6d95bb815f0d98420b647bc66ab0c495e8
Micael Cid <micaelcid10@gmail.com>
2024-08-30 02:48:58 +0100

    remove whitespace

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -121,7 +121,7 @@ class _MyAppState extends State<MyApp> {
   }
 
   Future<Album> fetchAlbum() async {
-    final httpClient = kIsWeb ? http.Client() :  InstrumentedHttpClient(http.Client());
+    final httpClient = kIsWeb ? http.Client() : InstrumentedHttpClient(http.Client());
 
     Random random = new Random();
     var id = random.nextInt(100);
